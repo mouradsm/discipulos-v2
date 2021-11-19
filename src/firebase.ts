83e028a824700c1039545f4a1737e2a8064c29f8
Diego Moura <diegosouza20049@hotmail.com>
2021-11-18 22:18:23 -0300

    adding firebase and some basic css styles

---
+++ src/firebase.ts
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCVlKbWO_eXYRcd5CXjlaWq85DWeiPpEuQ",
+  authDomain: "christian-manager-develop.firebaseapp.com",
+  databaseURL: "https://christian-manager-develop.firebaseio.com",
+  projectId: "christian-manager-develop",
+  storageBucket: "christian-manager-develop.appspot.com",
+  messagingSenderId: "157784816613",
+  appId: "1:157784816613:web:f4f4a2e527f7c8669f0d0c",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+
+const db = firebaseApp.firestore()
+const auth = firebaseApp.auth()
+
+export default { db, auth }
